5b6ff1e290fece94d3a8cf9ab6ef008ad06f6f2b
Robert Paterno <bobpaterno@gmail.com>
2014-07-25 15:15:13 -0500

    Still working on Year, not there yet

--- lib/year.rb
+++ lib/year.rb
@@ -1,10 +1,18 @@
+
 class Year
   attr_accessor :year
+  LINE_WIDTH = 64
+  NUM_MONTHS = 12
+  NUM_COLUMNS= 3
+  NUM_ROWS   = 4
+  MONTH_WIDTH= 20
+
   def initialize(year)
-    unless (1800..3000).include? year
+    unless (1800..3000).include? year.to_i
       raise ArgumentError, "cal: year #{year} not in range 1800...3000"
     end
-    @year = year
+    @year = year.to_i
+    @months = []
   end
 
 
@@ -14,6 +22,58 @@ class Year
       return true if @year % 400 == 0
     end
     return false
+  end
+
+
+  def header
+    "#{@year}".center(LINE_WIDTH-1).rstrip << "\n\n"
+  end
+
+
+  def make_months
+    NUM_MONTHS.times do |month|
+      @months << Month.new(month+1, @year)
+    end
+  end
+
+
+  def months(n)
+    @months[n-1]
+  end
+
+
+  def make_months_array
+    months = []
+    NUM_ROWS.times.with_index do |i|
+      m1 = Month.new((i*3)+1,@year).to_s.split("\n")
+      m2 = Month.new((i*3)+2,@year).to_s.split("\n")
+      m3 = Month.new((i*3)+3,@year).to_s.split("\n")
+      m1.collect! { |m| m.ljust(MONTH_WIDTH) }
+      m2.collect! { |m| m.ljust(MONTH_WIDTH) }
+      m3.collect! { |m| m.ljust(MONTH_WIDTH) }
+
+      months << m1[1..m1.size].zip(m2[1..m1.size],m3[1..m1.size])
+    end
+    months.flatten.flatten
+  end
+
+
+  def to_s
+    # make_months
+    output = ""
+    output << header
+    # months_array = @months.collect { |month| month.split("\n") }
+
+    # @months.each_slice(NUM_COLUMNS) do |row|
+    #   row.each { |month| output << (month.name.center(20) << "  ")}
+    #   output << (output.rstrip << "\n")
+    # end
+    months=make_months_array
+    months.each_slice(3) do |line|
+      output << line.join("  ")
+      output << "\n"
+    end
+    output
   end
 
 end
--- spec/year_spec.rb
+++ spec/year_spec.rb
@@ -31,7 +31,62 @@ RSpec.describe Year do
       actual = `./cal 1000`
       actual.should == expected
     end
+    it "prints error message about invalid range" do
+      expected = "cal: year 3002 not in range 1800...3000\n"
+      actual = `./cal 3002`
+      actual.should == expected
+    end
+
   end
 
+  context "Year#header method" do
+    it "prints out header year with appropriate width" do
+      expected = "                             2017\n\n"
+      year = Year.new(2017)
+      actual = year.header
+      actual.should == expected
+    end
+    it "prints out header year with appropriate width" do
+      expected = "                             1812\n\n"
+      year = Year.new(1812)
+      actual = year.header
+      actual.should == expected
+    end
+    it "prints out header year with appropriate width" do
+      expected = "                             1908\n\n"
+      year = Year.new(1908)
+      actual = year.header
+      actual.should == expected
+    end
+  end
+
+  context "Year#make_months method" do
+    it "checks that Year@months array is created" do
+      year = Year.new(2001)
+      expected =<<EOS
+   February 2001
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28
+
+EOS
+      year.make_months
+      actual = year.months(2).to_s
+      actual.should == expected
+    end
+  end
+
+  context "Year#make_months_array" do
+    it "checks that the method splits months by newlines" do
+      year = Year.new(2001)
+      expected = []
+      year.make_months
+      actual = year.make_months_array[0].inspect
+      actual.should == expected
+    end
+  end
 
 end
